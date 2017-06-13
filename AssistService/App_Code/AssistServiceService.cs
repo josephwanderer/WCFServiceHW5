5aa3e74ba6d5cf99cc0c25e0044b6b13cc2e8959
JosephWanderer <banhstopjw@gmail.com>
2017-06-13 11:18:21 -0700

    Add Project

---
+++ AssistService/App_Code/AssistServiceService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AssistServiceService" in code, svc and config file together.
+public class AssistServiceService : IAssistServiceService
+{
+
+    Community_AssistEntities db = new Community_AssistEntities();
+
+    public int Login(string email, string password)
+    {
+        int key = 0;
+        
+        
+        int result = db.usp_Login(email, password);
+
+        if(result != -1)
+        {
+            var userkey = (from k in db.People
+                           where k.PersonEmail.Equals(email)
+                           select k.PersonKey).FirstOrDefault();
+
+            key = (int)userkey;
+        }
+
+        return key;
+    }
+
+    public bool RegisterPerson(string lastname, string firstname, string email, string password, string aptnumber, string street, string city, string state, string zipcode, string homephone, string workphone)
+    {
+
+        bool result = true;
+
+        db.usp_Register(lastname, firstname, email, password, aptnumber, street, city, state, zipcode, homephone, workphone);
+
+        return result;
+    }
+
+    public int RequestGrant(int granttype, string requestexplanation, decimal amount, int userkey)
+    {
+        
+        int result = db.usp_AddRequest(granttype, requestexplanation, amount, userkey);
+        return result;
+    }
+
+    public List<GrantRequestInfo> ViewGrants(int userkey)
+    {
+        
+
+        var grantRequests = from rqs in db.GrantRequests
+                            where rqs.PersonKey == userkey
+                     select rqs;
+
+        List<GrantRequestInfo> requestsInfo = new List<GrantRequestInfo>();
+        foreach(var rq in grantRequests)
+        {
+                     
+
+                GrantRequestInfo grqst = new GrantRequestInfo();
+                grqst.GrantRequestAmount = (decimal)rq.GrantRequestAmount;
+                grqst.GrantRequestDate = rq.GrantRequestDate;
+                grqst.GrantRequestExplanation = rq.GrantRequestExplanation;
+                
+
+                grqst.GrantReviews = new List<GrantReview>();
+
+                foreach (var review in rq.GrantReviews)
+                {
+                    GrantReview grv = new GrantReview();
+                    grv.GrantRequestStatus = review.GrantRequestStatus;
+                    grv.GrantAllocationAmount = review.GrantAllocationAmount;
+                    grv.GrantReviewDate = review.GrantReviewDate;
+                    
+
+                    grqst.GrantReviews.Add(grv);
+                }
+
+                    
+
+                requestsInfo.Add(grqst);
+            
+        }
+
+        return requestsInfo;
+    }
+}
+
+
---
+++ AssistService/App_Code/IAssistServiceService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAssistServiceService" in both code and config file together.
+[ServiceContract]
+public interface IAssistServiceService
+{
+    [OperationContract]
+    bool RegisterPerson(string lastname, string firstname, string email, string password, string aptnumber, string street, string city, string state, string zipcode, string homephone, string workphone);
+
+    [OperationContract]
+    int Login(string email, string password);
+
+    [OperationContract]
+    int RequestGrant(int granttype, string requestexplanation, decimal amount, int userkey);
+
+    [OperationContract]
+    List<GrantRequestInfo> ViewGrants(int userkey);
+
+
+}
+
+[DataContract]
+public class GrantRequestInfo
+{
+    [DataMember]
+    public DateTime GrantRequestDate { get; set; }
+    [DataMember]
+    public string GrantRequestType { get; set; }
+    [DataMember]
+    public string GrantRequestExplanation { get; set; }
+    [DataMember]
+    public decimal GrantRequestAmount { get; set; }
+    [DataMember]
+    public List<GrantReview> GrantReviews { get; set; }
+
+}
+
